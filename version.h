2a6352f2cbafcbab3af7abf0cf421570d9f0f9af
Alex Zolotarev <me@alex.bio>
2017-01-27 08:14:31 +0300

    Increased version.

--- version.h
+++ version.h
@@ -5,7 +5,7 @@
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               1
 #define VERSION_REVISION            0
-#define VERSION_BUILD               2
+#define VERSION_BUILD               3
 
 #define VER_FILE_DESCRIPTION_STR    "Simple fswatch replacement."
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
